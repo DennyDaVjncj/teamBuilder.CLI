b171d0f825981555b61ec07a560d19fb18a6795a
Denny da Vjncj <daVjncjCode@gmail.com>
2020-11-16 18:58:45 -0500

    building out foundational class

--- develop/lib/Employee.js
+++ develop/lib/Employee.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee{//this class needs to set some base values
+    constructor(name,id,eMail){
+        this.name=name;
+        this.id=id;
+        this.eMail=eMail;
+    }
+    getName(){
+        this.name=`${data.name}`;//'data' being a hypothetical anchor value
+        console.log(this.name);
+    }
+}
+
+
+
+
+//this file serves to support instances thereof. never to be redered
