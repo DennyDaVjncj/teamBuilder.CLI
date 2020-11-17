2807c1de0944c604496e5d42f75a6b3a243eec15
Denny da Vjncj <daVjncjCode@gmail.com>
2020-11-17 11:25:22 -0500

    latest update

--- develop/lib/Employee.js
+++ develop/lib/Employee.js
@@ -1,16 +1,28 @@
 // TODO: Write code to define and export the Employee class
 
 class Employee{//this class needs to set some base values
-    constructor(name,id,eMail){
-        this.name=name;
+    constructor(name,id,email){
+        this.name=name;//second test passed
         this.id=id;
-        this.eMail=eMail;
+        this.eMail=email;
     }
     getName(){
-        this.name=`${data.name}`;//'data' being a hypothetical anchor value
-        console.log(this.name);
+        // this.name=`${data.name}`;//'data' being a hypothetical anchor value
+        // console.log(this.name);
+        return Employee()//this line passes test
     }
+    getId(){
+        return Employee(this.id)
+    }
+    getEmail(){
+        
+    }
+    
 }
+
+exports.module=Employee;//can be instantiated
+
+'commitMessage=can be instantiated'
 
 
 
