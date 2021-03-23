14bfa1d4d27fc902a3ef000dcaab89c9a73b69cb
Denny da Vjncj <daVjncjCode@gmail.com>
2021-03-22 20:39:48 -0400

    removed directory

--- vitamins/w9.minPrjct/a.multClsss/item.js
+++
@@ -1,8 +0,0 @@
-class Item {
-  constructor(title, price) {
-    this.title = title;//dynamic values, accounting for instances/instanciations
-    this.price = price;
-  }
-}
-
-module.exports = Item;
